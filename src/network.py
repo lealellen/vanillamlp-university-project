a23484ad1f8cd0bc0d4a0b5ea02ff8bfea9c6b11
lealellen <ellensleal@gmail.com>
2025-04-27 20:36:54 -0300

    constroi o init

--- src/network.py
+++ src/network.py
@@ -1,10 +1,54 @@
 import numpy as np
 import pandas as pd
+import random
 
 class MLP:
     def __init__(self, input_size, hidden_layers, output_size, learning_rate=0.01, epochs=1000):
-        # Inicializar os parâmetros da rede (pesos, bias, etc.)
-        pass
+        
+        """     in_weigths 
+            Pesos entrada -> camada escondida
+            O resultado vai mais ou menos isso
+            [0.5, 0.5, 0.5], # Pesos do neurônio de entrada 1 para os 3 neurônios escondidos
+            [0.5, 0.5, 0.5], # Pesos do neurônio de entrada 2 para os 3 neurônios escondidos
+            [0.5, 0.5, 0.5] # Pesos do neurônio de entrada 3 para os 3 neurônios escondidos
+        """
+        self.in_weigths = [[random.uniform(-1, 1) for _ in range(hidden_layers)] for _ in range(input_size)]
+        
+        
+        """     out_weigths 
+            # Pesos camada escondida -> saída
+            O resultado vai mais ou menos isso
+            [0.5, 0.5, 0.5], # Pesos do neurônio escondido 1 para os 3 neurônios da saída
+            [0.5, 0.5, 0.5], # Pesos do neurônio escondido 2 para os 3 neurônios da saída
+            [0.5, 0.5, 0.5] # Pesos do neurônio escondido 3 para os 3 neurônios da saída
+        """
+        self.out_weigths = [[random.uniform(-1, 1) for _ in range(output_size)] for _ in range(hidden_layers)]
+
+        """     hidden_bias
+            # Bias camada escondida 
+            O resultado vai mais ou menos isso
+            [0, 0, 0] # Um bias para cada neurônio da camada escondida
+        """
+        self.hidden_bias = [random.uniform(-1, 1) for _ in range(hidden_layers)]
+
+        """     out_bias 
+            # Pesos camada de saída
+            O resultado vai mais ou menos isso
+            [0, 0, 0] # Um bias para cada neurônio de saída
+        """
+        self.out_bias = [random.uniform(-1, 1) for _ in range(output_size)]
+
+        # Atribuindo os parâmetros como atributos da classe
+
+        self.epochs = epochs
+
+        self.learning_rate = learning_rate
+
+        self.input_size = input_size
+
+        self.hidden_layers = hidden_layers
+
+        self.output_size = output_size
 
     def initialize_weights(self):
         # Inicializar pesos e biases com valores aleatórios
@@ -15,11 +59,21 @@ class MLP:
         pass
 
     def activation_derivative(self, z):
-        # Derivada da função de ativação
+        """Função para calculo da derivada da sigmoide que é usada no cálculo do gradiente durante o backpropagation.
+            Ela permite ajustar os pesos da rede neural com base no erro da previsão.
+        """
         pass
 
     def forward(self, X):
         # Propagação para frente (forward pass)
+        pass
+
+    def fit(self, X, y):
+        # Treinamento da rede neural (loop de epochs)
+        pass
+
+    def predict(self, X):
+        # Realizar predições com a rede treinada
         pass
 
     def compute_loss(self, y_true, y_pred):
@@ -34,15 +88,10 @@ class MLP:
         # Atualizar os pesos usando os gradientes calculados
         pass
 
-    def fit(self, X, y):
-        # Treinamento da rede neural (loop de epochs)
-        pass
-
-    def predict(self, X):
-        # Realizar predições com a rede treinada
-        pass
-
     def accuracy(self, y_true, y_pred):
+        """
+        Realiza o calculo da acurácia, fazendo a divisão do número de previsões corretas pelo número total de previsões feitas
+        """
         acuraccy = y_true/y_pred
         return acuraccy
 
